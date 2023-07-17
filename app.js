f01df8194800f34b193caef7f42f0491f1450e1f
Tyler Jackson <87100297+tyler1588@users.noreply.github.com>
2023-07-16 22:43:10 -0400

    complete 4.9

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
 
 
---
+++ tests/note_api.test.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+        "title": "Test",
+        "author": "Test Author",
+        "url": "www.test.com",
+        "likes": 10
+    },
+    {
+        "title": "Test 2",
+        "author": "Test Author 2",
+        "url": "www.test2.com",
+        "likes": 20
+    },
+  ]
+  
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
