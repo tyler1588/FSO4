42b0e6fd118cbed409005cf26a7c4bb0ef276837
Tyler Jackson <tyler1588@users.noreply.github.com>
2023-07-20 20:09:34 -0400

    complete 4.18

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware)
 
 module.exports = app
